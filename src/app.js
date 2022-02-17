88c72dfa6f106fd9f66ec140d41357139467a878
Dagi <dagi.zewdu.dz@gmail.com>
2022-02-17 13:51:11 +0300

    Fixed Comments

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
-import { AddtoList, removeCompleted, setCompleted } from './js/controller/action';
+import {
+  AddRemoveEditForm,
+  AddtoList, editTodos, removeCompleted, removeUnCompleted, setCompleted,
+} from './js/controller/action';
 import RenderList from './js/controller/render-list';
 import { getFromStorage } from './js/controller/storage';
 import {
@@ -7,14 +10,54 @@ import {
 import './styles/style.css';
 
 let Todos = getFromStorage();
+let editInputForm = [];
 // checkbox addEventlistener
-const addCompletedEventListener = () => {
+const addEventListeners = () => {
   const checkboxs = selectMultipleDom('input.checkbox');
+  const editButton = selectMultipleDom('div.three-dots');
+  const checkButton = selectMultipleDom('p.success');
+  const editForms = selectMultipleDom('form.edit-form');
+  const editInputs = selectMultipleDom('input.edit-input');
+
+  checkButton.forEach((button) => {
+    button.addEventListener('click', () => {
+      Todos = setCompleted(button.id, false, Todos);
+      RenderList(Todos, editInputForm);
+      addEventListeners();
+    });
+  });
   checkboxs.forEach((checkbox) => {
     checkbox.addEventListener('change', () => {
       Todos = setCompleted(checkbox.id, checkbox.checked, Todos);
-      RenderList(Todos);
-      addCompletedEventListener();
+      RenderList(Todos, editInputForm);
+      addEventListeners();
+    });
+  });
+  editButton.forEach((button) => {
+    button.addEventListener('click', () => {
+      editInputForm = AddRemoveEditForm(button.id, editInputForm);
+      RenderList(Todos, editInputForm);
+      addEventListeners();
+    });
+  });
+  editInputs.forEach((input) => {
+    const value = Todos.find((todo) => todo.index.toString() === input.id);
+    input.value = value ? value.description : '';
+  });
+
+  editForms.forEach((form) => {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      editInputs.forEach((input) => {
+        if (input.id === form.id) {
+          const { value } = input;
+          const todos = editTodos(input.id, value, Todos);
+          Todos = todos;
+          editInputForm = AddRemoveEditForm(input.id, editInputForm);
+          RenderList(Todos, editInputForm);
+          addEventListeners();
+        }
+      });
     });
   });
 };
@@ -24,24 +67,27 @@ const handleSubmit = (e) => {
   e.preventDefault();
   const todos = AddtoList(Input.value, Todos);
   Todos = todos;
-  RenderList(todos);
-  addCompletedEventListener();
+  RenderList(todos, editInputForm);
+  addEventListeners();
   Input.value = '';
 };
-RenderList(Todos);
-addCompletedEventListener();
+RenderList(Todos, editInputForm);
+addEventListeners();
 
 // clear completed button
 btnClearCompleted.addEventListener('click', () => {
   const todos = removeCompleted(Todos);
+  RenderList(todos, editInputForm);
+  addEventListeners();
   Todos = todos;
 });
 
 // reset icon
 resetIcon.addEventListener('click', () => {
-  Todos = [];
-  RenderList(Todos);
-  addCompletedEventListener();
+  const todos = removeUnCompleted(Todos);
+  RenderList(todos, editInputForm);
+  addEventListeners();
+  Todos = todos;
 });
 
 form.addEventListener('submit', handleSubmit);
--- src/js/controller/action.js
+++ src/js/controller/action.js
@@ -1,4 +1,3 @@
-import RenderList from './render-list';
 import { AddtoStorage } from './storage';
 
 export const ArrangeList = (list) => list.map((item, index) => ({ ...item, index }));
@@ -19,6 +18,30 @@ export const setCompleted = (id, value, list) => {
 export const removeCompleted = (list) => {
   const todos = ArrangeList(list.filter((todo) => !todo.completed));
   AddtoStorage(todos);
-  RenderList(todos);
   return todos;
+};
+
+export const CheckForm = (id, editFormIdList) => {
+  const form = editFormIdList.find((index) => index === id.toString());
+  return form;
+};
+
+export const AddRemoveEditForm = (id, editFormIdList) => {
+  const check = CheckForm(id, editFormIdList);
+  if (check) { return editFormIdList.filter((index) => index !== id.toString()); }
+  return [...editFormIdList, id];
+};
+
+export const removeUnCompleted = (list) => {
+  const todos = ArrangeList(list.filter((todo) => todo.completed));
+  AddtoStorage(todos);
+  return todos;
+};
+
+export const editTodos = (id, description, todos) => {
+  const todo = todos.find((t) => t.index.toString() === id);
+  const Todo = { ...todo, description };
+  const newTodos = todos.map((t) => (t.index.toString() === id ? Todo : t));
+  AddtoStorage(newTodos);
+  return newTodos;
 };
